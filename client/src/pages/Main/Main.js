9cdecaf10083c6dcf23536a3d611f5b32fcb4a2d
suhye0n <contact.suhyeon@gmail.com>
2023-10-12 17:09:25 +0900

    메인 페이지 추가

---
+++ client/src/pages/Main/Main.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    background: url('/bg.jpg');
+    background-repeat: no-repeat;
+    background-size: contain;
+`;
+
+const Main = () => {
+
+    return (
+        <Container>
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+            <br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게<br/>길게
+        </Container>
+    );
+};
+
+export default Main;
